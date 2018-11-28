d65c6e92ac6f2b3ecbbc5e1053bae838c3874e11
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-11-27 21:06:50 -0800

    Create server.js/ full-stack app

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+var port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
