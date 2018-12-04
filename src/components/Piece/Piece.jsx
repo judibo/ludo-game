5038e8501bdcc9d6fb85680badf4ef89a6583eca
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-12-04 00:16:20 -0800

    Dynamic style Home-Pieces

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -3,11 +3,11 @@ import './Piece.css';
 import gameService from '../../utils/gameService';
 
 
-const Piece = ({game, piece, color}) => {
+const Piece = ({game, piece, color, user}) => {
   return (
     <div className="Piece">
       <div className="Piece-color" 
-                style={{backgroundColor: game.waitingToMove ? 'white' : color}}
+                style={{backgroundColor: game.waitingToMove && ( piece.player === game.playerIndex )? 'white' : color}}
                 onClick={() => gameService.handleMovePosition(piece._id)}/>
     </div>
   );
